bbc50c39acf0564e0a2b216e90a9eac3faedabee
javpot <vernetjaven@gmail.com>
2023-08-08 13:12:58 -0400

    Initial Commit

---
+++ todoApp2Swift/todoApp2SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoApp2SwiftApp.swift
+//  todoApp2Swift
+//
+//  Created by Javen Vernet on 2023-08-08.
+//
+
+import SwiftUI
+
+@main
+struct todoApp2SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
